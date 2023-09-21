75a3dfe7cec59c4f6618096dd64fef4551fdf83c
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-09-21 13:29:06 -0600

    Cambios video 314
    Se crea nuevo componente footer, se crea su html y css, se registra el nuevo componente en el app module,
    se coloca el navbar en color oscuro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component'; //añadimos el nuevo componente 
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
+  //siempre se deben poner los nuevo componentes aqui 
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
