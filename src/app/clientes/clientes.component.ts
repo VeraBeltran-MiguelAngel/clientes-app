bfbcdd4dd3c33c6850a3e3bbc7b5f961a672361e
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-09-21 17:06:55 -0600

    Cambios video 318
    Listando los objetos dle tipo cliente
    Se crea la tabla con la info de los clientes en el clientes.c.html
    y se colocan los valores del arreglo en clientes.c.ts

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,9 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-clientes',
-  templateUrl: './clientes.component.html'
+  templateUrl: './clientes.component.html',
 })
-export class ClientesComponent {
+export class ClientesComponent implements OnInit {
+  //para poder decir que el objeto de de tipo Cliente hay que importar la clase
+  clientes: Cliente[] = [
+    {
+      id: 1,
+      nombre: 'Andres',
+      apellido: 'Guzman',
+      email: 'profesor@bolsadeideas.com',
+      createAt: '2017-12-11',
+    },
+    {
+      id: 2,
+      nombre: 'Andres2',
+      apellido: 'Guzman2',
+      email: 'profesor2@bolsadeideas.com',
+      createAt: '2018-12-11',
+    },
+    {
+      id: 3,
+      nombre: 'Andres3',
+      apellido: 'Guzman3',
+      email: 'profesor3@bolsadeideas.com',
+      createAt: '2019-12-11',
+    },
+    {
+      id: 4,
+      nombre: 'Andres4',
+      apellido: 'Guzman4',
+      email: 'profesor4@bolsadeideas.com',
+      createAt: '2020-12-11',
+    },
+    {
+      id: 5,
+      nombre: 'Andres5',
+      apellido: 'Guzman5',
+      email: 'profesor5@bolsadeideas.com',
+      createAt: '2021-12-11',
+    },
+    {
+      id: 6,
+      nombre: 'Andres6',
+      apellido: 'Guzman6',
+      email: 'profesor6@bolsadeideas.com',
+      createAt: '2022-12-11',
+    },
+    {
+      id: 7,
+      nombre: 'Andres7',
+      apellido: 'Guzman7',
+      email: 'profesor7@bolsadeideas.com',
+      createAt: '2023-12-11',
+    },
+    {
+      id: 8,
+      nombre: 'Andres8',
+      apellido: 'Guzman8',
+      email: 'profesor8@bolsadeideas.com',
+      createAt: '2024-12-11',
+    }
+  ];
+  constructor() {}
 
+  ngOnInit(): void {}
 }
