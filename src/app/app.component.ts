b1840150d6da8e2f2f3b11faa5122aef7076dc94
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-09-21 11:00:17 -0600

    Cambios del video 308
    Se agregan variables de texto al componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+//componente prncipal
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a Angular';
+  curso: string = 'Curso Spring 5 con Angular 7';
+  profesor: string = 'Andres guzman';
 }
