e11629130b6ed99c913fcd7ad96fdc28603a400d
Miguel Angel Vera Beltrán <miguel-angel.vera-beltran@outlook.com>
2023-09-21 22:28:08 -0600

    Cambios video 321
    Implementando Observable en nuestra clase Servicio ClienteService
    Esto con la funcionalidad de hacer reactiva la aplicacion (que reciba peticiones asincronas)
     (Datos asíncronos sin necesidad de recargar la página tanto para enviar como para recibir)
    
     Actualizamos una libreria llamada tslib debido a un error de versiones al clonar el rep
     en mi pc asus

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
-//usaremos la constante declarada en el json
+//usaremos la constante declarada en el json(arreglo de clientes)
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
+//para poder manejar peticiones en tiempo real (asincrono)
+import { Observable } from 'rxjs';
+// el metodo of es para crear el flujo observable
+import { of } from 'rxjs';
 
 //esta anotacion es solo para clases de tipo servicio
-//(logica de negocio) se puede inyectar a otros componentes via inyeccion de dependencias
+//(logica de negocio) se puede inyectar a otros componentes via inyeccion de dependencias a una clase component
 @Injectable({
   providedIn: 'root',
 })
 export class ClienteService {
   constructor() {}
 
-  //como usas arreglo de tipo cliente hay que importar la clase 
-  getClientes(): Cliente[] {
-    return CLIENTES;
+  /*como usas arreglo de tipo cliente hay que importar la clase 
+  lo que retorna el metodo tiene que ser un stream (flujo de datos) por lo tanto un observable de clientes*/
+  getClientes(): Observable<Cliente[]> {
+    //tenemos que convertir el arreglo de clientes en un observable para que coincidan los tipos
+    // -- Convertimos / creamos nuestro flujo observable a partir de los objetos clientes
+    return of(CLIENTES);
   }
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -10,13 +10,36 @@ export class ClientesComponent implements OnInit {
   //para poder decir que el objeto de de tipo Cliente hay que importar la clase
   clientes: Cliente[] | undefined;
 
-//aqui se define el atributo y se inyecta su valor
+  /*tenemos que inyectar la clase de servicio con inyeccion de dependencias
+  aqui se define el nombre del atributo(clienteService) y se inyecta su valor*/
   constructor(private clienteService:ClienteService) {}
 
   ngOnInit(): void {
     //en este metodo se ejecutan procesos al iniciar el componente
 
-    //aqui asignamos el metodo get clientes de la clase de servicio 
-    this.clientes = this.clienteService.getClientes();
+    /*Tenemos que suscribir o registrar el observador a nuestros clientes.
+    Por lo tanto getClientes() es un observable (observado por observadores)*/
+    this.clienteService.getClientes().subscribe(
+      /*dentro de este metodo (suscribir) el observador justamente sería asignar en el atributo
+        clientes el valor que se está recibiendo desde el clienteService que sería el listado 
+        de clientes con los cambios.Entonces acá tenemos una función anónima nuestro Salvador 
+        que se encarga de asignar el valor al cliente component*/
+
+        //clientes es el argumento (el resultado del stream), se asigna el parametro a this clientes
+         /*esta linea seria el observador , por lo tanto actualiza el listado de clientes
+         desde el cliente component y eso se pasa a la plantilla(a la vista con los posibles cambios)*/
+        clientes => this.clientes = clientes 
+    
+        //el argumento es el valor que se emite desde el flujo reactivo que retorna el 
+        //servicio(esta en cliente.service) (Observable<Cliente[]>), 
+
+        /*function(clientes){ la funcion => es igual a esto
+          this se usa para referenciar a la variable global o a nivel de clase
+          this.clientes = clientes
+        } */
+
+       
+
+    );
   }
 }
