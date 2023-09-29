2da716ef618410d11de26c3160c23a8596b501b6
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-09-29 11:59:16 -0600

    Cambios video 345
    Instalar SweetAlert2 para enviar mensajes de alerta en el cliente
    
    Se descarga libreria de sweet alert
    Al configurar json sort dentro de visual este ordena alfabeticamente el contenido de los json del proyecto
    Se intenta arreglar el footer para que no se mueva con el contenido no quedo :(

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { Router } from '@angular/router';
+//libreria para mensajes de alerta mas bonitos
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -25,8 +27,17 @@ export class FormComponent implements OnInit {
 
     //llamando al metodo crear(pasamos el objeto cliente y lo vamos a suscribir)
     this.clienteService.create(this.cliente).subscribe(
-      //para redirigir al listado de clientes y subir los cambios
-      (response) => this.router.navigate(['/clientes'])
+      //para redirigir al listado de clientes y mostrar el nuevo registro,
+      // y mostrar un mensaje de confirmacion de cliente creado
+      (cliente) => {
+        this.router.navigate(['/clientes']);
+        //creamos el alert con tres parametros (titulo,`mensaje`,tipo de mensaje)
+        swal.fire(
+          'Nuevo Cliente',
+          `Cliente: ${cliente.nombre} creado con exito!`,
+          'success'
+        );
+      }
     );
   }
 }
